ccd4fe720ea3d30a6a350a61ca42c7e315c4b6ac
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2023-04-14 16:14:42 -0500

    feat: Change file location

--- src/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DATABASE}?retryWrites=true&w=majority`
-  )
-  .then(() => console.log('DB connected'))
-  .catch(err => console.log(err));
